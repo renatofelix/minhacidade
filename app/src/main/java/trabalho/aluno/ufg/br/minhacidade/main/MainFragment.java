ac4016e12bdd685bf61111160025cd829118d801
Jonathas Eloi <jonathaseloi@gmail.com>
2018-06-19 15:02:10 -0300

    - Modificação nas telas

--- app/src/main/java/trabalho/aluno/ufg/br/minhacidade/main/MainFragment.java
+++ app/src/main/java/trabalho/aluno/ufg/br/minhacidade/main/MainFragment.java
@@ -2,16 +2,26 @@ package trabalho.aluno.ufg.br.minhacidade.main;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import butterknife.BindView;
 import trabalho.aluno.ufg.br.minhacidade.R;
+import trabalho.aluno.ufg.br.minhacidade.modelos.Problema;
 
 public class MainFragment extends Fragment {
 
     private int idXML = R.layout.fragments_envios;
     private String nomeFragment;
+
+    private List<Problema> problemas = new ArrayList<>();
+    @BindView(R.id.rv_problemas)
+    private RecyclerView recyclerView;
 
     public MainFragment() {
     }
@@ -37,8 +47,12 @@ public class MainFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        View view = inflater.inflate(idXML, container, false);
+
+//        recyclerView.setAdapter();
+
         // Inflate the layout for this fragment
-        return inflater.inflate(idXML, container, false);
+        return view;
     }
 
 }
---
+++ app/src/main/java/trabalho/aluno/ufg/br/minhacidade/modelos/Problema.java
@@ -0,0 +1,8 @@
+package trabalho.aluno.ufg.br.minhacidade.modelos;
+
+/**
+ * Created by johnn on 19/06/2018.
+ */
+
+public class Problema {
+}
