219141928f4595c38a430b5fcf34ebfd2d1d5ab9
Jonathas Eloi <jonathaseloi@gmail.com>
2018-06-22 10:47:28 -0300

    - Selecionar tipo do problema

--- app/src/main/java/trabalho/aluno/ufg/br/minhacidade/CadastroProblemaActivity.java
+++ app/src/main/java/trabalho/aluno/ufg/br/minhacidade/CadastroProblemaActivity.java
@@ -1,13 +1,38 @@
 package trabalho.aluno.ufg.br.minhacidade;
 
+import android.app.Dialog;
+import android.content.DialogInterface;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import butterknife.BindView;
+import butterknife.OnClick;
+import trabalho.aluno.ufg.br.minhacidade.utils.Utils;
 
 public class CadastroProblemaActivity extends AppCompatActivity {
+
+    @BindView(R.id.tvTipoProblema)
+    protected TextView tvTipoProblema;
+
+    private Dialog alertDialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cadastro_problema);
+    }
+
+    @OnClick
+    public void selecionarTipoProblema(View view) {
+        alertDialog = Utils.onCreateSelectDialog(this, R.string.strTituloTipoProblema, R.array.tiposProblemas, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                //salvar tipo na variavel problema
+            }
+        });
+        alertDialog.show();
     }
 }
--- app/src/main/java/trabalho/aluno/ufg/br/minhacidade/utils/Utils.java
+++ app/src/main/java/trabalho/aluno/ufg/br/minhacidade/utils/Utils.java
@@ -1,7 +1,14 @@
 package trabalho.aluno.ufg.br.minhacidade.utils;
 
+import android.app.Activity;
+import android.app.Dialog;
+import android.content.DialogInterface;
+import android.os.Bundle;
 import android.support.design.widget.Snackbar;
+import android.support.v7.app.AlertDialog;
 import android.view.View;
+
+import trabalho.aluno.ufg.br.minhacidade.R;
 
 /**
  * Created by johnn on 19/06/2018.
@@ -12,5 +19,12 @@ public class Utils {
     public static void createSnackbar(View view, String mensagem){
         Snackbar snackbar = Snackbar.make(view ,mensagem, Snackbar.LENGTH_LONG);
         snackbar.show();
+    }
+
+    public static Dialog onCreateSelectDialog(Activity activity, int idTitulo, int idArray, DialogInterface.OnClickListener onClickListener) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(activity);
+        builder.setTitle(idTitulo)
+                .setItems(idArray, onClickListener);
+        return builder.create();
     }
 }
